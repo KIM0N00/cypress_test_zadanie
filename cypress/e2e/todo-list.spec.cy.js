37f0e8935b50bb5d9736568e9f3528012d55788b
Vitaly Belyavsky <belyavskiivitalik@inbox.ru>
2023-09-25 14:59:37 +0300

    1
    Выполнено

---
+++ cypress/e2e/todo-list.spec.cy.js
@@ -0,0 +1,31 @@
+describe("ToDo List Test", () => {
+  it("should add, mark as completed, and delete items from the list", () => {
+    cy.visit("https://forhemer.github.io/React-Todo-List/");
+
+    // Добавляем 3 элемента в ToDo лист
+    cy.get(".input-text").type("Item 1{enter}");
+    cy.get(".input-text").type("Item 2{enter}");
+    cy.get(".input-text").type("Item 3{enter}");
+
+    // Проверяем, что элементов в листе стало 3
+    cy.get('li').should('have.length', 3);
+
+    /* cy.get('input[type="checkbox"]').eq(0).click();*/
+
+    // Помечаем первый элемент как выполненный
+    cy.get('.TodoItem_checkbox__Tf0FQ').eq(0).click();
+
+
+    // Получаем текст первого элемента
+    cy.get(".TodoItem_item__iFWQW").eq(0).invoke("text").then((text) => {
+      // Проверяем, что текст содержит зачеркивание
+      expect(text).to.include("Item 1");
+    });
+
+    // Удаляем второй элемент из списка
+    cy.get('button').eq(2).click({ force: true });
+
+    // Проверяем, что второй элемент больше не отображается в списке
+    cy.get("li").should("have.length", 2);
+  });
+});
